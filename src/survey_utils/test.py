ced27705ba3b6eaca8208fd20b4d30f46f7230f4
Andreas Paepcke <paepcke@cs.stanford.edu>
2016-05-20 17:36:28 -0700

    Factored unfold into its own file and class.

--- src/survey_utils/test.py
+++ src/survey_utils/test.py
@@ -3,37 +3,45 @@ Created on May 19, 2016
 
 @author: paepcke
 '''
-import unittest
-from utils import unfold
+#****import unittest
+from unfold import unfold
 
-class TestSurveyUtils(unittest.TestCase):
-
-
-    def setUp(self):
-        self.survey = [['userId','question','questionType','timeAdded','answer'],
-                       [10,'DOB','pullDown','Jun2010','1983'],     
-                       [10,'gender','radio','May2011','F'],
-                       [20,'DOB','pullDown','Jun2010','1980'],
-                       [20,'gender','radio','May2011','M']
-                       ]
-        
-        self.surveyBadConst =\
-                        [['userId','question','questionType','timeAdded','answer'],
-                         [10,'DOB','radio','Jun2010','1983'],   # 'radio' should be 'pullDown'     
-                         [10,'gender','radio','May2011','F'],
-                         [20,'DOB','pullDown','Jun2010','1980'],
-                         [20,'gender','radio','May2011','M']
-                        ]
-
-    def tearDown(self):
-        pass
-
-
-    def test_unfold_no_error(self):
-        unfold(self.survey, 'question', 'answer')
+# class TestSurveyUtils(unittest.TestCase):
+# 
+# 
+#     def setUp(self):
+#         self.survey = [['userId','question','questionType','timeAdded','answer'],
+#                        [10,'DOB','pullDown','Jun2010','1983'],     
+#                        [10,'gender','radio','May2011','F'],
+#                        [20,'DOB','pullDown','Jun2010','1980'],
+#                        [20,'gender','radio','May2011','M']
+#                        ]
+#         
+#         self.surveyBadConst =\
+#                         [['userId','question','questionType','timeAdded','answer'],
+#                          [10,'DOB','radio','Jun2010','1983'],   # 'radio' should be 'pullDown'     
+#                          [10,'gender','radio','May2011','F'],
+#                          [20,'DOB','pullDown','Jun2010','1980'],
+#                          [20,'gender','radio','May2011','M']
+#                         ]
+# 
+#     def tearDown(self):
+#         pass
+# 
+# 
+#     def test_unfold_no_error(self):
+#         unfold(self.survey, 'question', 'answer')
 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
+    survey = [['userId','question','questionType','timeAdded','answer'],
+                   [10,'DOB','pullDown','Jun2010','1983'],     
+                   [10,'gender','radio','May2011','F'],
+                   [20,'DOB','pullDown','Jun2010','1980'],
+                   [20,'gender','radio','May2011','M']
+                   ]
     
-    unittest.main()
+    unfold(survey, 'question', 'answer')
+    
+    #****unittest.main()
