808fd6ac817d8d5e47c5ae55b8c69ed56187c543
Andreas Paepcke <paepcke@cs.stanford.edu>
2016-05-26 08:40:12 -0700

    Setup install --modules extension finally working.

--- setup.py
+++ setup.py
@@ -1,26 +1,56 @@
-import multiprocessing
 from setuptools import setup
 
+from distutils.command.install import install
 import os,sys
+
 sys.path.append(os.path.join(os.getcwd(), 'src'))
 
-# Modify mods_to_install to taste.
-# If math_utils included, then scipy/numpy
-# and matplotlib will be installed as well.
-# Modules like 'unfold' are lighter weight:
+mods_to_install = []
 
-#mods_to_install = ['unfold']
-mods_to_install = ['unfold','math_utils']
+class InstallCommand(install):
+
+    description = "Installs the unfolding package."
+    user_options = [
+        ('module=', 'm', "Specify 'unfolding', 'math_utils', or 'all'."),
+    ] + install.user_options
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        # *Must* initialize any options you introduced
+        # in user_options. Else parent's finalize_options
+        # won't read the option from the command line at
+        # all!!!!
+        self.module = None
+
+    def finalize_options(self):
+        install.finalize_options(self)
+        assert self.module in (None, 'unfolding', 'math_utils', 'all'), 'Invalid module!'
+        
+    def run(self):
+        if self.module == 'unfolding':
+            mods_to_install = ['unfold']
+        elif self.module in ['math_utils', 'all']:
+            mods_to_install = ['unfold', 'math_utils']
+        else:
+            # No modules arg given on the command line.
+            # Require it for the install command, so that
+            # user doesn't unexpectedly install all of
+            # numpy/scipy:
+         
+            print("Must provide --modules [unfolding | math_utils | all]. Install aborted.")
+            sys.exit()
+        
+        install.run(self)
 
 mods_to_require = []
 if 'unfold' in mods_to_install:
-  mods_to_require.append('ordered-set>=2.0.1')
+    mods_to_require.append('ordered-set>=2.0.1')
 if 'math_utils' in mods_to_install:
-  mods_to_require.extend(['scipy>=0.17.0',
-			  'matplotlib>=1.5.0',
-			  'pandas>=0.17.1',
-			  'freetype-py>=1.0.2',
-			  ])
+    mods_to_require.extend(['scipy>=0.17.0',
+                            'matplotlib>=1.5.0',
+                            'pandas>=0.17.1',
+                            'freetype-py>=1.0.2',
+                            ])
 
 test_requirements = ['nose>=1.0']
 
@@ -28,11 +58,14 @@ setup(
     name = "survey_utils",
     version = "0.0.1",
     py_modules = mods_to_install,
+    cmdclass = {
+      'install': InstallCommand
+      },
 
     # Dependencies on other packages:
     setup_requires   = ['nose>=1.3.7',
-			'numpy>=1.11.0'
-			],
+            'numpy>=1.11.0'
+            ],
     tests_require    = test_requirements,
     install_requires = mods_to_require + test_requirements,
 
